0bd3a6fa72ff3788843b9d69c1f1089664a1a30b
João Marcos Gris <joaomarcosgris@hotmail.com>
2023-08-30 11:11:21 -0300

    fix: create client on every request

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,14 @@ import { users } from './schema';
 
 const DATABASE_AUTH_TOKEN=""
 const DATABASE_URL="http://127.0.0.1:8080"
-const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
-export const db = drizzle(client);
 
 export const app = new Hono();
 
 app.get('/users', async (ctx) => {
   try {
+    const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
+    const db = drizzle(client);
+
     const results =  await db.select().from(users).all()
     console.log(results)
     return ctx.json(results);
--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,7 @@
 import { sql } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const users = sqliteTable('users', {
+export const users = sqliteTable('example_users', {
 	id: integer('id').primaryKey(),
 	name: text('name').notNull(),
 	email: text('email').notNull(),
